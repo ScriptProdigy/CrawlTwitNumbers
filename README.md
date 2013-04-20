58cf09fac5545d5f3e4d46962a9ca4dea9fab97f
Matt Smith <matt.joseph.smith@gmail.com>
2013-04-20 14:56:09 -0400

    Implemented Twitter Public Stream printing. Going to start processing them for phone numbers.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 CrawlTwitNumbers
 ================
 
-Crawl your twitter account/s for people's cell numbers and maybe other information? Why not. 
+Crawl twitter's public status update stream for random phone numbers because why not. Data + More Data = Fun.
+
+Set Up
+======
+
+Create the file 'twitter_account.txt' in the same directory as the program. Then inside of the file, write 'Username:Password' for your twitter account, with NO quotes. Then simply run.
--- main.py
+++ main.py
@@ -1,3 +1,34 @@
 from twitter import *
+import time
+
+class CrawlTwitNumbers:
+    def __init__(self):
+
+        UserPassFile = open("twitter_account.txt", "r").read()
+        Username = str(UserPassFile.split(":")[0])
+        Password = str(UserPassFile.split(":")[1].rstrip())
+
+        self.TwitterAuth = UserPassAuth(Username, Password)
+        self.Stream = TwitterStream(auth=self.TwitterAuth)
+
+        self.StreamIterator = self.Stream.statuses.sample()
+
+    def run(self):
+        for tweet in self.StreamIterator:
+            self.processTweet(tweet)
 
 
+    def processTweet(self, tweet):
+        try:
+            print ""
+            print tweet['user']['screen_name'] + " : " + tweet['text']
+            print ""
+        except:
+            pass
+
+    def addtodb(self, username, number):
+        print username + "'s number is " + number
+
+if(__name__ == "__main__"):
+    crawl = CrawlTwitNumbers()
+    crawl.run()

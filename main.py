823dc80bfa143558862943d019dfe8a8b9565be9
Matt Smith <matt.joseph.smith@gmail.com>
2013-04-20 15:21:36 -0400

    Basic phone number searching implemented. A FUCK TON of false positives. Have to work on that.. Ugh.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from twitter import *
+import re
 import time
 
 class CrawlTwitNumbers:
     def __init__(self):
+
+        self.Regex_Pattern = "(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})"
+        self.Regex = re.compile(self.Regex_Pattern)
 
         UserPassFile = open("twitter_account.txt", "r").read()
         Username = str(UserPassFile.split(":")[0])
@@ -17,17 +21,24 @@ class CrawlTwitNumbers:
         for tweet in self.StreamIterator:
             self.processTweet(tweet)
 
-
     def processTweet(self, tweet):
         try:
-            print ""
-            print tweet['user']['screen_name'] + " : " + tweet['text']
-            print ""
+
+            Numbers = ""
+            NumberResults = self.Regex.findall(tweet['text'])
+            
+            for number in NumberResults:
+                Numbers += number + " "
+
+
+            if(len(NumberResults) > 0):
+                print tweet['user']['screen_name'] + " : " + tweet['text'] + " : " + str(Numbers)
+
         except:
             pass
 
     def addtodb(self, username, number):
-        print username + "'s number is " + number
+        pass
 
 if(__name__ == "__main__"):
     crawl = CrawlTwitNumbers()
